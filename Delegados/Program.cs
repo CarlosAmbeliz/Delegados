be04eb45e6638809951266cdfca145bb35deb22f
Carlos Ambeliz <carlosambeliz@hotmail.com>
2020-09-05 02:21:25 -0600

    Creacion del proyecto

---
+++ Delegados/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegados
+{
+    class Program
+    {
+        delegate void Suma(int numero1, int numero2);
+        static void Main(string[] args)
+        {
+        }
+    }
+}
